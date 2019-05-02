3c4e990265d0200c01975c01acd7e900f8806090
Aris Mantes <admantes.com>
2019-05-03 00:38:38 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';  
 import './App.css';
+import Todos from './Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      curval: "",
+      todos: []
+    }  
+  }
+  
+ 
+
+  handleChange = (e)=>{
+    this.setState({
+      curval: e.target.value
+    })
+  }
+
+  onSubmit  = (e) => {
+    e.preventDefault();
+    this.setState({
+      todos: [...this.state.todos, this.state.curval],
+      curval: ""
+     })
+  }
+
+  componentDidUpdate(){
+    console.log(this.state.todos);
+  }
+  render()
+  {
+    return (
+      <div className="App">
+      
+          <h1>Ultimate TodoList</h1>
+          <form onSubmit={this.onSubmit}>
+            <input value={this.state.curval} onChange={this.handleChange} /> 
+            <input type="submit" />  
+          </form>
+          <Todos todos={this.state.todos}></Todos>
+      </div>
+    );
+  }
 }
 
 export default App;
